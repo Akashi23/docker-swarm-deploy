4e08ac21138bda1065a8c32596c9676ef4e5673d
Akashi23 <dan.zh.1999@gmail.com>
2022-12-10 11:46:13 +0600

    test 3

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,10 @@
 output "docker-swarm-1-ipv4" {
   description = "Droplet ipv4"
-  value       = digitalocean_droplet.docker-swarm-1.ipv4_address_private
+  value = "123"
+#   value       = digitalocean_droplet.docker-swarm-1.ipv4_address_private
 }
 
-output "docker-swarm-2-ipv4" {
-  description = "Droplet ipv4"
-  value       = digitalocean_droplet.docker-swarm-2.ipv4_address_private
-}
+# output "docker-swarm-2-ipv4" {
+#   description = "Droplet ipv4"
+#   value       = digitalocean_droplet.docker-swarm-2.ipv4_address_private
+# }
