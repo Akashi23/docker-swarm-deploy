e05eadaa3e48a2e6293dd5d1ff7bdd90a71d1d09
Akashi23 <dan.zh.1999@gmail.com>
2022-12-10 11:27:58 +0600

    test 1

---
+++ terraform/docker-swarm-deploy.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "s3" {
+    endpoint                = "fra1.digitaloceanspaces.com"
+    key                     = "terraform.tfstate"
+    bucket                  = "terraformstatesaves"
+    region                  = "us-west-1"
+    skip_requesting_account_id = true
+    skip_credentials_validation = true
+    skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
+  }
+}
+
+resource "digitalocean_droplet" "docker-swarm-1" {
+  image  = "docker-20-04"
+  name   = "docker-swarm-1"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = ["37027018"]
+}
+
+resource "digitalocean_droplet" "docker-swarm-2" {
+  image  = "docker-20-04"
+  name   = "docker-swarm-2"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = ["37027018"]
+}
+
+output "docker-swarm-1-ipv4" {
+  description = "Droplet ipv4"
+  value       = digitalocean_droplet.docker-swarm-1.ipv4_address_private
+}
+
+output "docker-swarm-2-ipv4" {
+  description = "Droplet ipv4"
+  value       = digitalocean_droplet.docker-swarm-2.ipv4_address_private
+}
