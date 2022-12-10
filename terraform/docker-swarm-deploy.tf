15fe7925cf2614882ff88ac5e884010241ccb8de
Akashi23 <dan.zh.1999@gmail.com>
2022-12-10 11:42:15 +0600

    test 2

--- terraform/docker-swarm-deploy.tf
+++ terraform/docker-swarm-deploy.tf
@@ -26,13 +26,3 @@ resource "digitalocean_droplet" "docker-swarm-2" {
   size   = "s-1vcpu-1gb"
   ssh_keys = ["37027018"]
 }
-
-output "docker-swarm-1-ipv4" {
-  description = "Droplet ipv4"
-  value       = digitalocean_droplet.docker-swarm-1.ipv4_address_private
-}
-
-output "docker-swarm-2-ipv4" {
-  description = "Droplet ipv4"
-  value       = digitalocean_droplet.docker-swarm-2.ipv4_address_private
-}
