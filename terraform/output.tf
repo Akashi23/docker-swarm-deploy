900fb34e9292bb29eb65b72db9463e633c1935ed
Akashi23 <dan.zh.1999@gmail.com>
2022-12-13 14:21:37 +0600

    fix

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
-output "docker-swarm-1-ipv4" {
+output "docker_swarm_1_ipv4" {
   description = "Droplet ipv4"
   value       = digitalocean_droplet.docker-swarm-1.ipv4_address_private
 }
 
-output "docker-swarm-2-ipv4" {
+output "docker_swarm_2_ipv4" {
   description = "Droplet ipv4"
   value       = digitalocean_droplet.docker-swarm-2.ipv4_address_private
 }
